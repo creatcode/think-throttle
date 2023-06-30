91e672ccb124a320d2e405d007abc2bc4506abd4
justlike <872864670@qq.com>
2023-06-30 11:20:10 +0800

    Update config.php
    更新默认配置

--- src/config.php
+++ src/config.php
@@ -12,7 +12,7 @@ return [
     'prefix' => 'apithrottle_',
     // 缓存的键，true 表示使用来源ip
     'key' => true,
-    // 要被限制的请求类型, eg: GET POST PUT DELETE HEAD 等
+    // 要被限制的请求类型, eg: GET POST PUT DELETE HEAD 等（*表示所有类型请求）
     'visit_method' => ['GET', 'HEAD'],
     // 设置访问频率，例如 '10/m' 指的是允许每分钟请求10次;'10/60'指允许每60秒请求10次。值 null 表示不限制， eg: null 10/m  20/h  300/d 200/300
     'visit_rate' => '100/m',
